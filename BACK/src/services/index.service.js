e3145f7325e762345b9add81328936a543bf7b8f
unknown <romerolorenzo661@gmail.com>
2026-05-07 12:23:28 -0300

    instanciación de repositorios con 'new' en index.service.js

--- BACK/src/services/index.service.js
+++ BACK/src/services/index.service.js
@@ -7,7 +7,7 @@ import ReserveRepository from "../repository/reserve.repository.js";
 import UsersRepository from "../repository/users.repository.js";
 
 export const classService = new ClassRepository();
-export const membershipService = new MembershipRepository(); // Corregido de membershipServer a membershipService
+export const membershipService = new MembershipRepository(); // -Corregido de membershipServer a membershipService 
 export const paymentService = new PaymentRepository();
 export const planService = new PlanRepository();
 export const recurrentScheduleService = new RecurrentScheduleRepository();
