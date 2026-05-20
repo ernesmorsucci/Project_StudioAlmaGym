11444755ed62e314ba50a08380d6463af6c8997a
ErnesDev <ernesmorsucci14@gmail.com>
2026-05-20 12:39:59 -0300

    Fix en api config

--- FRONT/src/services/api.js
+++ FRONT/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.BACKEND_URL || 'http://localhost:8080/', // La URL de tu backend
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
   withCredentials: true // ¡Súper importante! Esto permite que el navegador guarde la cookie JWT
 });
 
