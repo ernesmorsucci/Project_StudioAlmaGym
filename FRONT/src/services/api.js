0ec785ddb51f9d5adb1b74047c1fa60db04f2fa3
ErnesDev <ernesmorsucci14@gmail.com>
2026-05-20 12:21:20 -0300

    Variable de entorno para url server

--- FRONT/src/services/api.js
+++ FRONT/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // La URL de tu backend
+  baseURL: process.env.BACKEND_URL || 'http://localhost:8080/', // La URL de tu backend
   withCredentials: true // ¡Súper importante! Esto permite que el navegador guarde la cookie JWT
 });
 
